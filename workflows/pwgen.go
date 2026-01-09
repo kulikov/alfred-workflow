75e742bf740ebd20873c2d751b9f1fbd72632660
Dmitry Kulikov <kulikov.dm@gmail.com>
2026-01-09 11:51:52 +0000

    Pwgen: diversify password presets

--- workflows/pwgen.go
+++ workflows/pwgen.go
@@ -8,12 +8,34 @@ import (
 func Pwgen(length int) []Item {
 	rand.Seed(time.Now().UnixNano())
 	chars := "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
-	output := make([]Item, 10)
+	charsLower := "abcdefghijklmnopqrstuvwxyz0123456789"
+	charsUpper := "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+	output := make([]Item, 12)
 
-	for i := 0; i < 10; i++ {
-		password := make([]byte, length)
-		for j := 0; j < length; j++ {
-			password[j] = chars[rand.Intn(len(chars))]
+	for i := 0; i < 12; i++ {
+		l := length
+		c := chars
+
+		if i < 2 {
+			// standard long
+		} else if i < 4 {
+			c = charsLower
+		} else if i < 6 {
+			c = charsUpper
+		} else if i < 8 {
+			l = length / 2
+		} else {
+			l = length / 2
+			c = charsLower
+		}
+
+		if l < 1 {
+			l = 1
+		}
+
+		password := make([]byte, l)
+		for j := 0; j < l; j++ {
+			password[j] = c[rand.Intn(len(c))]
 		}
 
 		passwordStr := string(password)
